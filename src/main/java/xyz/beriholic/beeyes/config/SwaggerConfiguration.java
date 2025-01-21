de8f858ef15ec6877ab9f29df642df73d0985407
Beriholic <beriholic@petalmail.com>
2025-01-21 13:09:21 +0800

    refactor: :recycle:(swagger): remove custom open api login config

--- src/main/java/xyz/beriholic/beeyes/config/SwaggerConfiguration.java
+++ src/main/java/xyz/beriholic/beeyes/config/SwaggerConfiguration.java
@@ -7,24 +7,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.parameters.QueryParameter;
-import io.swagger.v3.oas.models.responses.ApiResponse;
-import io.swagger.v3.oas.models.responses.ApiResponses;
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import xyz.beriholic.beeyes.entity.RestBean;
-import xyz.beriholic.beeyes.entity.vo.response.AuthorizeVO;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Configuration
 @SecurityScheme(type = SecuritySchemeType.HTTP, scheme = "Bearer",
@@ -47,52 +33,5 @@ public class SwaggerConfiguration {
                         .description("我的博客")
                         .url("https://beriholic.xyz")
                 );
-    }
-
-    @Bean
-    public OpenApiCustomizer customerGlobalHeaderOpenApiCustomizer() {
-        return api -> this.authorizePathItems().forEach(api.getPaths()::addPathItem);
-    }
-
-    private Map<String, PathItem> authorizePathItems() {
-        Map<String, PathItem> map = new HashMap<>();
-        map.put("/api/auth/login", new PathItem()
-                .post(new Operation()
-                        .tags(List.of("登录校验相关"))
-                        .summary("登录验证接口")
-                        .addParametersItem(new QueryParameter()
-                                .name("username")
-                                .required(true)
-                        )
-                        .addParametersItem(new QueryParameter()
-                                .name("password")
-                                .required(true)
-                        )
-                        .responses(new ApiResponses()
-                                .addApiResponse("200", new ApiResponse()
-                                        .description("OK")
-                                        .content(new Content().addMediaType("*/*", new MediaType()
-                                                .example(RestBean.success(new AuthorizeVO()).asJsonString())
-                                        ))
-                                )
-                        )
-                )
-        );
-        map.put("/api/auth/logout", new PathItem()
-                .get(new Operation()
-                        .tags(List.of("登录校验相关"))
-                        .summary("退出登录接口")
-                        .responses(new ApiResponses()
-                                .addApiResponse("200", new ApiResponse()
-                                        .description("OK")
-                                        .content(new Content().addMediaType("*/*", new MediaType()
-                                                .example(RestBean.success())
-                                        ))
-                                )
-                        )
-                )
-
-        );
-        return map;
     }
 }
