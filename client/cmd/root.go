1cfb3a6d6182b8a7870063326433b1f3456d21b4
Beriholic <beriholic@petalmail.com>
2025-03-03 12:58:07 +0800

    feat: :sparkles:(client): add a cool logo when the client starts

--- client/cmd/root.go
+++ client/cmd/root.go
@@ -9,11 +9,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const logo = `
+ ________  _______   _______       ___    ___ _______   ________      
+|\   __  \|\  ___ \ |\  ___ \     |\  \  /  /|\  ___ \ |\   ____\     
+\ \  \|\ /\ \   __/|\ \   __/|    \ \  \/  / | \   __/|\ \  \___|_    
+ \ \   __  \ \  \_|/_\ \  \_|/__   \ \    / / \ \  \_|/_\ \_____  \   
+  \ \  \|\  \ \  \_|\ \ \  \_|\ \   \/  /  /   \ \  \_|\ \|____|\  \  
+   \ \_______\ \_______\ \_______\__/  / /      \ \_______\____\_\  \ 
+    \|_______|\|_______|\|_______|\___/ /        \|_______|\_________\
+                                 \|___|/                  \|_________|
+`
+
 var rootCmd = &cobra.Command{
 	Use:   "beeyesc",
 	Short: "BeEyes Client",
 	Long:  `BeEyes Client`,
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print(logo)
+
 		slog.Info("开始运行系统监测")
 		if err := internal.RunMonitor(); err != nil {
 			slog.Info("运行系统监测出错", "err", err)
